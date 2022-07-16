44501e90827ec743e850c21ab600a79b9361be84
SOHAIL1996 <sorox23@gmail.com>
2022-07-16 16:29:49 +0200

    Testing textX

---
+++ src/property_based_tester/property_based_language_generation/test_specification.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------------- 
+Property-Based Language Generator based on textX.
+----------------------------------------------------
+Supervisor: Prof. Dr. Nico Hochgeschwender
+            Prof. Dr. Paul Ploger
+            Sven Schneider 
+
+Author    : Salman Omar Sohail
+----------------------------------------------------
+Date: July 16, 2022
+----------------------------------------------------
+"""
+
+import nltk

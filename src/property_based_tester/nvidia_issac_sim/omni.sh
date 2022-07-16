dcdc87206942a1dd17613fce8b72fe4d3edadda1
SOHAIL1996 <sorox23@gmail.com>
2022-07-16 16:29:05 +0200

    Adding omniverse interface

---
+++ src/property_based_tester/nvidia_issac_sim/omni.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash /home/sorox23/.local/share/ov/pkg/isaac_sim-2022.1.0/python.sh /home/sorox23/robotic_ws/master_thesis_ws/src/property_based_tester/src/property_based_tester/nvidia_issac_sim/omniverse.py

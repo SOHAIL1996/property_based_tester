ce5a405ce07bc4ba474534a40bbfbfb9510dbe14
SOHAIL1996 <sorox23@gmail.com>
2022-08-08 20:54:13 +0200

    iso 2348-1 template

---
+++ src/property_based_tester/standards_tests/iso_23482_1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------------- 
+Tests compliant to ISO 23482-1
+----------------------------------------------------
+Supervisor: Prof. Dr. Nico Hochgeschwender
+            Prof. Dr. Paul Ploger
+            Sven Schneider 
+
+Author    : Salman Omar Sohail
+----------------------------------------------------
+Date: July 01, 2022
+----------------------------------------------------
+"""
+
+import rospy
+from gazebo_msgs.msg import ContactsState
+
+from property_based_tester.configuration.config import Configuration
+from property_based_tester.properties.primitive_properties import PrimitiveProperties
+
+class StandardTesting():
+
+    def __init__(self) -> None:
+        pass
+
+    def section_7_2(self):
+        pass
+
+    def section_11(self):
+        pass
+
+    def section_13_1(self):
+        pass
+
+    def section_13_2(self):
+        pass
+
+    def section_13_3(self):
+        pass
+
+    def section_13_4(self):
+        pass
+
+    def section_14_2(self):
+        pass
+
+    def section_15_1(self):
+        pass
+
+    def section_15_2(self):
+        pass
+
+    def section_15_3(self):
+        pass
+
+    def section_15_4(self):
+        pass
+
+    def section_16(self):
+        pass
+
+    def section_17(self):
+        pass

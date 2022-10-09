0bd4b0ce348247c9d9d90269be185e36b1690189
SOHAIL1996 <sorox23@gmail.com>
2022-10-09 02:30:30 +0200

    xARM6 ISO 23482-1 Sec 7.2 Implementation

--- src/property_based_tester/tests/pblg_test_interface_manip.py
+++ src/property_based_tester/tests/pblg_test_interface_manip.py
@@ -22,11 +22,11 @@ import numpy as np
 import os, subprocess, signal, time
 import pytest
 import allure
+import time
 
 from property_based_tester.configuration.config import Configuration
 from property_based_tester.property_based_language_generation.textx_test_specification import PropertyBasedLanguageGenerator
-from property_based_tester.robot_controllers.navigation_client import single_goal_movebase
-from property_based_tester.robot_controllers.speed_client import move
+from property_based_tester.robot_controllers.xarm_moveit import ManipulatorMoveit
 from property_based_tester.scen_gen.obstacle_gen import Model
 from property_based_tester.scen_gen.force_generation import apply_force
 from property_based_tester.scen_gen.model_placement import delete_model
@@ -70,8 +70,11 @@ class TestScenario(Base):
     def test_set_up(self, pblg_config):
         """Initializing property-based generator language scenario.
         """  
-        rospy.init_node('pblg_test')
-        pytest.robot_controller = subprocess.Popen(['roslaunch', self.config.rospkg_name, self.config.launch_controller_file])
+        pytest.xarm6 = ManipulatorMoveit('xarm6')
+        pytest.xarm6_gripper = ManipulatorMoveit('xarm_gripper')
+
+        # Restarting robot control at start of each test
+        # pytest.robot_controller = subprocess.Popen(['roslaunch', self.config.rospkg_name, self.config.launch_controller_file])
 
     def test_scenario_generation(self, randomizer, pblg_config):
         
@@ -98,142 +101,188 @@ class TestScenario(Base):
                             Y= 0)
             robo.robot_pose()
 
+        # Correcting payload spawn
         try:
             payload = Model(pblg_config[2][0].scenario_modifier[0].sm_payload[0].payload,
-                            pblg_config[2][0].scenario_modifier[0].sm_payload[0].x_pos,
-                            pblg_config[2][0].scenario_modifier[0].sm_payload[0].y_pos,
-                            pblg_config[2][0].scenario_modifier[0].sm_payload[0].z_pos)
+                            x=pblg_config[2][0].scenario_modifier[0].sm_payload[0].x_pos,
+                            y=pblg_config[2][0].scenario_modifier[0].sm_payload[0].y_pos,
+                            z=pblg_config[2][0].scenario_modifier[0].sm_payload[0].z_pos,
+                            R= pblg_config[2][0].scenario_modifier[0].sm_payload[0].r_ori,
+                            P= pblg_config[2][0].scenario_modifier[0].sm_payload[0].p_ori,
+                            Y= pblg_config[2][0].scenario_modifier[0].sm_payload[0].y_ori)
             payload.insert_model()
         except:
             pass
 
+        # Correcting safety_obstacle spawn
         try:
             for i in range(len(pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle)):
                 safety_obstacle = Model(pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].safety_obstacle,
                                         pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].x_pos,
                                         pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].y_pos,
-                                        pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].z_pos)
+                                        pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].z_pos,
+                                        pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].r_ori,
+                                        pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].p_ori,
+                                        pblg_config[2][0].scenario_modifier[0].sm_safety_obstacle[i].y_ori)
                 safety_obstacle.insert_model()
         except:
             pass
             
         assert True
-
-
-    # def test_scenario_execution(self, randomizer, pblg_config): 
-    #     """Defines a scenario for the rest of the tests to run in using coodrinates.
-    #     """    
-    #     try:
-    #         # Test definition goal if exists
-    #         coord_x = pblg_config[2][0].scenario_modifier[0].sm_robot_goal[0].x_pos
-    #         coord_y = pblg_config[2][0].scenario_modifier[0].sm_robot_goal[0].y_pos
-    #         direction = pblg_config[2][0].scenario_modifier[0].sm_robot_goal[0].y_ori
-    #     except:
-    #         # Randomize given coordinates
-    #         coord_x, coord_y, direction = randomizer(-2,2),randomizer(-2,2),randomizer(0,360)
-
-    #     # Excute navigation and temporal logger
-    #     temporal_logger = subprocess.Popen(['rosrun', self.config.rospkg_name, 'temporal_log.py'], preexec_fn=os.setsid)
-
-    #     apply_force(x=0,y=40,z=0,link='base_link',timeout=2)
-
-    #     try:
-    #         if pblg_config[2][0].scenario_modifier[0].sm_robot_velocity[0].manual_speed == True:
-    #             result = move(pblg_config[2][0].scenario_modifier[0].sm_robot_velocity[0].robot_speed,
-    #                           self.config.robot_cmd_vel, 
-    #                           pblg_config[2][0].scenario_modifier[0].sm_robot_velocity[0].speed_duration)
-    #     except:
-    #         result = pose_action_client(coord_x, coord_y, direction, timeout=15)
-
-    #     os.killpg(os.getpgid(temporal_logger.pid), signal.SIGTERM) 
-
-    #     # Logging test collision details for other tests
-    #     pytest.collision = self.composite_properties.in_collision
-    #     pytest.collider_1 = self.composite_properties.collider_1
-    #     pytest.collider_2 = self.composite_properties.collider_2
-    #     pytest.collision_force = self.composite_properties.collision_force
-
-    #     assert result == True    
-
-    # def test_must_have_collision_force_less_than(self, pblg_config):
-    #     """ Checking the max force exerted by the robot on an obstacle.
-    #     """ 
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_have_collision_force_less_than':
-    #             check = True
-    #             assert self.composite_properties.must_have_collision_force_less_than(pytest.collision_force, configuration[1].force_threshhold) == True
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
     
-    # def test_must_collide(self, pblg_config):
-    #     """ Checking if the robot collided with a specific obstacle during navigation.
-    #     """    
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_collide':
-    #             check = True
-    #             assert pytest.collision == True
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
-
-    # def test_must_not_collide(self, pblg_config):
-    #     """ Checking if the robot has not collided with an obstacle during navigation.
-    #     """ 
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_not_collide':
-    #             check = True
-    #             assert pytest.collision == True
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
-
-    # def test_must_be_at(self, pblg_config):
-    #     """ Checking if the robot is within a given area.
-    #     """    
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_be_at':
-    #             check = True
-
-    #             assert self.composite_properties.must_be_at(target_area_min=[configuration[1].x1,configuration[1].y1,configuration[1].z1], 
-    #                                                         target_area_max=[configuration[1].x2,configuration[1].y2,configuration[1].z2]) == True
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
-
-    # def test_must_not_be_at(self, pblg_config):
-    #     """ Checking if the robot is not within a given area.
-    #     """    
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_not_be_at':
-    #             check = True
-
-    #             assert self.composite_properties.must_be_at(target_area_min=[configuration[1].x1,configuration[1].y1,configuration[1].z1], 
-    #                                                         target_area_max=[configuration[1].x2,configuration[1].y2,configuration[1].z2]) == True
+    def test_scenario_execution(self, randomizer, pblg_config): 
+        """Defines a scenario for the rest of the tests to run in using coodrinates.
+        """    
+        multi_goal = []
+        result = None
                 
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
-    
-    # def test_must_have_orientation(self, pblg_config):
-    #     """ Checking if the robot is has a given area.
-    #     """    
-    #     check = False
-    #     for configuration in pblg_config[2][1]:
-    #         if configuration[0] == 'must_have_orientation':
-    #             check = True
+        # Excute manipulation and temporal logger
+        temporal_logger = subprocess.Popen(['rosrun', self.config.rospkg_name, 'temporal_log.py'], preexec_fn=os.setsid)
 
-    #             assert self.composite_properties.must_have_orientation(object=configuration[1].entity, 
-    #                                                         orientation=[configuration[1].roll,
-    #                                                                      configuration[1].pitch,
-    #                                                                      configuration[1].yaw], 
-    #                                                         time=0, 
-    #                                                         tolerance=configuration[1].tolerance) == True
-    #     if check == False:
-    #         pytest.skip("Test Un-marked")
+        # Apply random forces
+        try:
+            apply_force(x=randomizer(-pblg_config[2][0].scenario_modifier[0].sm_imparted_forces[0].imparted_forces,
+                            pblg_config[2][0].scenario_modifier[0].sm_imparted_forces[0].imparted_forces),
+                        y=randomizer(-pblg_config[2][0].scenario_modifier[0].sm_imparted_forces[0].imparted_forces,
+                            pblg_config[2][0].scenario_modifier[0].sm_imparted_forces[0].imparted_forces),
+                        z=0,
+                        link= pblg_config[2][0].scenario_modifier[0].sm_imparted_forces[0].target_entity,
+                        timeout=5)
+        except:
+            pass
+
+        try:
+            for goals in pblg_config[2][0].scenario_modifier[0].sm_robot_goal:
+                multi_goal.append([goals.x_pos, goals.y_pos, goals.z_pos, goals.r_ori, goals.p_ori, goals.y_ori])
+            # Multi goal movebase control test
+            for goal in multi_goal:
+                result = pytest.xarm6.go_to_pose_goal(x =goal[0], y =goal[1], z =goal[2], R=goal[3] , P=goal[4], Y=goal[5])
+            pytest.xarm6.set_state('home') 
+        except:
+            pass
+
+                    
+        os.killpg(os.getpgid(temporal_logger.pid), signal.SIGTERM) 
+               
+        # Logging test collision details for other tests
+        pytest.collision = self.composite_properties.in_collision
+        pytest.collider_1 = self.composite_properties.collider_1
+        pytest.collider_2 = self.composite_properties.collider_2
+        pytest.collision_force = self.composite_properties.collision_force
+
+        # Apply dead time for robot slippage and checking stopping time
+        try:
+            time.sleep(pblg_config[2][0].scenario_modifier[0].sm_dead_time[0].dead_time)
+        except:
+            pass
+    
+        assert result == True    
+
+    def test_must_have_collision_force_less_than(self, pblg_config):
+        """ Checking the max force exerted by the robot on an obstacle.
+        """ 
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_have_collision_force_less_than':
+                check = True
+                assert self.composite_properties.must_have_collision_force_less_than(pytest.collision_force, configuration[1].force_threshhold) == True
+        if check == False:
+            pytest.skip("Test Un-marked")
+    
+    def test_must_collide(self, pblg_config):
+        """ Checking if the robot collided with a specific obstacle during manipulation.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_collide':
+                check = True
+                assert pytest.collision == True
+        if check == False:
+            pytest.skip("Test Un-marked")
+
+    def test_must_not_collide(self, pblg_config):
+        """ Checking if the robot has not collided with an obstacle during manipulation.
+        """ 
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_not_collide':
+                check = True
+                assert pytest.collision == False
+        if check == False:
+            pytest.skip("Test Un-marked")
+
+    def test_must_be_at(self, pblg_config):
+        """ Checking if the robot is within a given area.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_be_at':
+                check = True
+                assert self.composite_properties.must_be_at(target_area_min=[configuration[1].x1,configuration[1].y1,configuration[1].z1], 
+                                                            target_area_max=[configuration[1].x2,configuration[1].y2,configuration[1].z2]) == True
+        if check == False:
+            pytest.skip("Test Un-marked")
+
+    def test_must_not_be_at(self, pblg_config):
+        """ Checking if the robot is not within a given area.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_not_be_at':
+                check = True
+
+                assert self.composite_properties.must_be_at(target_area_min=[configuration[1].x1,configuration[1].y1,configuration[1].z1], 
+                                                            target_area_max=[configuration[1].x2,configuration[1].y2,configuration[1].z2]) == True
+                
+        if check == False:
+            pytest.skip("Test Un-marked")
+    
+    def test_must_be_near_to(self, pblg_config):
+        """ Checking if the robot is within a given euclidean distance.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_be_near_to':
+                check = True
+                assert self.composite_properties.must_be_near_to(object=self.config.robot_urdf, 
+                                                                target_object=configuration[1].entity, 
+                                                                req_dis=configuration[1].euclidean_distance, 
+                                                                tolerance=configuration[1].tolerance) == True
+        if check == False:
+            pytest.skip("Test Un-marked")
+    
+    def test_must_not_be_near_to(self, pblg_config):
+        """ Checking if the robot is within a given euclidean distance.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_not_be_near_to':
+                check = True
+                assert self.composite_properties.must_be_near_to(object=self.config.robot_urdf, 
+                                                                target_object=configuration[1].entity, 
+                                                                req_dis=configuration[1].euclidean_distance, 
+                                                                tolerance=configuration[1].tolerance) == True
+        if check == False:
+            pytest.skip("Test Un-marked")
+    
+    def test_must_have_orientation(self, pblg_config):
+        """ Checking if the robot is has a given area.
+        """    
+        check = False
+        for configuration in pblg_config[2][1]:
+            if configuration[0] == 'must_have_orientation':
+                check = True
+                assert self.composite_properties.must_have_orientation(object=configuration[1].entity, 
+                                                                       orientation=[configuration[1].roll,
+                                                                                    configuration[1].pitch,
+                                                                                    configuration[1].yaw], 
+                                                                       time=0, 
+                                                                       tolerance=configuration[1].tolerance) == True
+        if check == False:
+            pytest.skip("Test Un-marked")
 
     def test_static_obstacle_generation_tear_down(self, pblg_config):
-        """Tearing down the setup for navigation.
+        """Tearing down the setup for manipulation.
         """  
         delete_model(pblg_config[2][0].world_type) 
         
@@ -249,5 +298,22 @@ class TestScenario(Base):
         except:
             pass
 
-        pytest.robot_controller.terminate()
-        # allure.attach(data, 'Configuration', allure.attachment_type.CSV)            
+        # pytest.robot_controller.terminate()
+
+        # Attaching log file to the test results
+        test_parameters = self.config.workspace + '/src/property_based_tester/property_based_language_generation/test_definitions.pblg'
+        simulation_data = self.config.workspace + '/src/property_based_tester/temporal_cache/logs/test_logs.csv'
+        controller_parameters = self.config.workspace + '/src/property_based_tester/configuration/property_based_tester_params.yaml'
+
+        with open(test_parameters) as f:
+            test_data = f.read()
+        
+        with open(simulation_data) as f:
+            sim_data = f.read()
+
+        with open(controller_parameters) as f:
+            controller_data = f.read()
+
+        allure.attach(test_data, 'Test Parameters')
+        allure.attach(sim_data, 'Simulation Data')
+        allure.attach(controller_data, 'Controller Parameters')            

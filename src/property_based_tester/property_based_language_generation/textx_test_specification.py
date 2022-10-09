eaa3c900360c8c5e658308a9041179d8600469f1
SOHAIL1996 <sorox23@gmail.com>
2022-10-09 22:14:28 +0200

    Addition, update and fixes to random scenario gen.

--- src/property_based_tester/property_based_language_generation/textx_test_specification.py
+++ src/property_based_tester/property_based_language_generation/textx_test_specification.py
@@ -18,6 +18,7 @@ from textx import metamodel_from_file
 from property_based_tester.configuration.config import Configuration
 
 import pytest
+import random
 
 class PropertyBasedLanguageGenerator():
 
@@ -28,7 +29,9 @@ class PropertyBasedLanguageGenerator():
         self.scenario_composite_tests[2][0]: Contains section scenario information
         self.scenario_composite_tests[2][1]: Contains section composite properties information 
         """
-
+        # Standard Test Generator
+        self.scenario_composite_tests = []
+        
         self.config = Configuration()               
         
         try:
@@ -47,20 +50,24 @@ class PropertyBasedLanguageGenerator():
 
             for i in range(len(self.test_model.test_type[0].section_number)):
                 self.standard_sections.append(self.test_model.test_type[0].section_number[i].section)
+
+            for x in range(len(self.test_model.test_type[0].section_number)):
+
+                scenario_compo = self.scenario_composite_test_extractor(
+                                                    self.test_model.test_type[0].section_number[x].scenario_configuration,
+                                                    self.test_model.test_type[0].section_number[x].custom_scenario.property_check)
+                test_details = [self.standard, self.standard_sections[x], scenario_compo]
+
+                self.scenario_composite_tests.append(test_details)
         except:
-            print('No standard detected')
+            print('No standard detected using User Scenario')
+            for x in range(len(self.test_model.test_type)):
+                scenario_compo = self.scenario_composite_test_extractor(
+                                                    self.test_model.test_type[x].scenario_configuration,
+                                                    self.test_model.test_type[x].custom_scenario.property_check)
+                test_details = ['User Scenario', ' ', scenario_compo]
 
-        # Standard Test Generator
-        self.scenario_composite_tests = []
-        
-        for x in range(len(self.test_model.test_type[0].section_number)):
-
-            scenario_compo = self.scenario_composite_test_extractor(
-                                                self.test_model.test_type[0].section_number[x].scenario_configuration,
-                                                self.test_model.test_type[0].section_number[x].custom_scenario.property_check)
-            test_details = [self.standard, self.standard_sections[x], scenario_compo]
-
-            self.scenario_composite_tests.append(test_details)
+                self.scenario_composite_tests.append(test_details)        
 
     def scenario_composite_test_extractor(self, scenario, composite_tests):
 
@@ -94,5 +101,86 @@ class PropertyBasedLanguageGenerator():
                 comp.append(('must_not_be_near_to', composite_tests[i].must_not_be_near_to))
 
         return (scen, comp)
-             
-# p = PropertyBasedLanguageGenerator()
+
+class PropertyBasedLanguageGeneratorRandomizer():
+
+    def __init__(self):
+
+        self.scenario_composite_tests = []
+        self.config = Configuration()               
+        
+        try:
+            self.pblg = metamodel_from_file(self.config.grammar_dir)
+            self.test_model = self.pblg.model_from_file(self.config.test_def_dir)
+        except:
+            print('\n Test definitions directory not detected at {}'.format(self.config.test_def_dir))
+
+        
+        scenario_compo = self.scenario_composite_test_extractor(
+                                            self.test_model.test_type[0].scenario_configuration,
+                                            self.test_model.test_type[0].custom_scenario.property_check)
+        test_details = ['Random Scenario', ' ', scenario_compo]
+
+        self.scenario_composite_tests.append(test_details)
+
+        self.rando_scenario = self.random_scenario_generation()
+        print(self.rando_scenario[0][2])
+
+    def scenario_composite_test_extractor(self, scenario, composite_tests):
+
+        comp = []
+        scen = scenario
+
+        for i in range(len(composite_tests)):
+            
+            if composite_tests[i].must_not_collide != None:
+                comp.append(('must_not_collide', composite_tests[i].must_not_collide))
+
+            if composite_tests[i].must_collide != None:
+                comp.append(('must_collide', composite_tests[i].must_collide))
+
+            if composite_tests[i].must_be_at != None:
+                comp.append(('must_be_at', composite_tests[i].must_be_at))
+
+            if composite_tests[i].must_not_be_at != None:
+                comp.append(('must_not_be_at', composite_tests[i].must_not_be_at))
+
+            if composite_tests[i].must_have_collision_force_less_than != None:
+                comp.append(('must_have_collision_force_less_than', composite_tests[i].must_have_collision_force_less_than))
+
+            if composite_tests[i].must_have_orientation != None:
+                comp.append(('must_have_orientation', composite_tests[i].must_have_orientation))
+
+            if composite_tests[i].must_be_near_to != None:
+                comp.append(('must_be_near_to', composite_tests[i].must_be_near_to))
+
+            if composite_tests[i].must_not_be_near_to != None:
+                comp.append(('must_not_be_near_to', composite_tests[i].must_not_be_near_to))
+
+        return (scen, comp)
+
+    def random_scenario_generation(self):
+
+        random_scen = []
+        comp_props = []
+        worlds = []
+        obst = []
+
+        iter = self.scenario_composite_tests[0][2][0].iterations
+        
+        for i in range(len(self.scenario_composite_tests[0][2][1])):
+            # print(self.scenario_composite_tests[0][2][1][i][0])
+            comp_props.append(self.scenario_composite_tests[0][2][1][i])
+
+        for i in range(len(self.scenario_composite_tests[0][2][0].worlds)):
+            worlds.append(self.scenario_composite_tests[0][2][0].worlds[i].world_type)
+
+        for i in range(len(self.scenario_composite_tests[0][2][0].obstacles)):
+            obst.append(self.scenario_composite_tests[0][2][0].obstacles[i].obstacle_type)
+
+        for i in range(iter):
+            random_scen.append([random.choice(worlds), obst, comp_props])
+        
+        return random_scen
+
+p = PropertyBasedLanguageGeneratorRandomizer()

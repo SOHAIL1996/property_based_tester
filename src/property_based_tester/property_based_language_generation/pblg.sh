1ab83cf29c2db5d36093fb8ac0de88f1c58f34e5
SOHAIL1996 <sorox23@gmail.com>
2022-11-29 17:39:12 +0100

    remove old named convention dsl

--- src/property_based_tester/property_based_language_generation/pblg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm test_definitions.dot test_definitions.dot.png dsl_grammar.dot
-textx generate test_definitions.pblg --grammar dsl_grammar.tx --target dot
-textx generate dsl_grammar.tx --target dot
-dot -Tpng -O test_definitions.dot
